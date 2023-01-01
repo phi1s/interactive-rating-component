b43c1a539a91123b313ad320a49ef825b6c525d3
philipp <>
2023-01-01 17:29:23 +0100

    working version

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const inputView = document.getElementById("input-view");
 const resultView = document.getElementById("result-view");
 const btn = document.getElementById("submit-btn");
+const ratings = document.getElementsByClassName("btn");
+const actualRating = document.getElementById("rating");
+
+Array.from(ratings).forEach((rating) => {
+  rating.addEventListener("click", () => {
+    actualRating.innerHTML = rating.innerHTML;
+  });
+});
 
 btn.onclick = function () {
   inputView.style.display = "none";
